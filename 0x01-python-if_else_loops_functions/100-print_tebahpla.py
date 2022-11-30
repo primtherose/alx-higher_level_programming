e2559c22096e3da900786a12a2a946f28f228e15
primrose <primtherose66@gmail.com>
2022-11-30 19:03:36 +0000

    Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-i = 0
-for c in range(ord('z'),ord('a') - 1, -1)
-    print("{}",format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+for i in range(ord('z'), ord('a') - 1, -1):
+    print('{}'.format(chr(i - 32) if (i % 2 != 0) else chr(i)), end='')
